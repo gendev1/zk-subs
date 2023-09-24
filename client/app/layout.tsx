55ea9a21d821bcdc4310960a6cf3e2b6e0e593cf
sagred <sagred17@gmail.com>
2023-09-24 08:20:02 -0400

    fasd

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "../components/theme-provider";
 import { Header } from "components/header";
 import { ThirdwebProvider, metamaskWallet } from "@thirdweb-dev/react";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ZK-Subs",
-  description: "Anon Private Subscriptions",
-};
 
 export default function RootLayout({
   children,
