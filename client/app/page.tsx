0f462595f3a97d9218f8d2b8b1c5be8b443c7a60
sagred <sagred17@gmail.com>
2023-09-24 08:08:53 -0400

    asdg

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import Main from "components/pages/main";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    alert("This application is only connected to frontend and blockchain, zkp server is not yet connected")
+  },[])
+
   return (
     <div>
       <Main />
